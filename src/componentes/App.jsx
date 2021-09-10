ddd5074cf0e45a25f2018bf024970888f8e31d2c
Luis Corrales Marin <luiscorrales5012@gmail.com>
2021-09-10 16:54:55 -0500

    .

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const App = () => {
     return (
         <div>
-            <h1>Main</h1>
+            <h1 className="p-6 max-w-sm mx-auto bg-red-200 rounded-xl shadow-md flex items-center space-x-4">Main</h1>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './componentes/App';
 
 
-// Framework Tailwind
-import './index.css';
 
 
 ReactDOM.render(
